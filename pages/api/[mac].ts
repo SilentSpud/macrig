c7c6a500e34613e2237114ba11ed2f3c6e46ef64
Silent Spud <1478389+SilentSpud@users.noreply.github.com>
2021-12-01 18:05:05 -0700

    make generic

--- pages/api/[mac].ts
+++ pages/api/[mac].ts
@@ -6,29 +6,12 @@ type Data = {
   name: string
 }
 
-const MacRegex = /^([0-9a-fA-F]{2}:){5}([0-9a-fA-F]{2})$/;
-const SlimMacRegex = /^[0-9a-fA-F]{12}$/;
-const ThreeRegex = /^([0-9a-fA-F]{3}:){5}([0-9a-fA-F]{2})$/;
-const ThreeDotRegex = /^([0-9a-fA-F]{3}\.){5}([0-9a-fA-F]{2})$/;
-
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  const rawMac = req.url?.toString().replace(/\/api\//g, "") ?? "";
-  let mac: string = "";
-  if (MacRegex.test(rawMac)) {
-    mac = rawMac;
-  }
-  if (SlimMacRegex.test(rawMac)) {
-    mac = rawMac.replace(/(..)(..)(..)(..)(..)(..)/g, (_substr, ...args) => {
-      const newArgs = [...args];
-      newArgs.pop();
-      newArgs.pop();
-      return newArgs.join(":");
-    })
-  }
+  const mac = req.url?.toString().replace(/\/api\//g, "") ?? "";
 
   const macInfo = oui(mac);
-  res.status(200).send(macInfo ?? "MAC Not Found");
+  res.status(200).send(macInfo.split("\n")[0] ?? "MAC Not Found");
 }
