4339a82f88781389f4e421d68ebcb821ffe16176
Silent Spud <1478389+SilentSpud@users.noreply.github.com>
2021-12-02 07:13:05 -0700

    one liner

--- pages/api/[mac].ts
+++ pages/api/[mac].ts
@@ -2,6 +2,5 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import oui from "oui";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  const mac = oui(req.url?.toString().replace(/\/api\//g, "")) ?? "";
-  res.status(200).send(mac.split("\n")[0] ?? "MAC Not Found");
+  res.status(200).send(oui(req.url?.toString().replace(/\/api\//g, "")).split("\n")[0] ?? "MAC Not Found");
 }
