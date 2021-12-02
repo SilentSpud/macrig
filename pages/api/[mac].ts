f670cfcbe3e121c9efd10763e6f7c9c5369d175c
Silent Spud <1478389+SilentSpud@users.noreply.github.com>
2021-12-01 18:10:05 -0700

    further reduce code

--- pages/api/[mac].ts
+++ pages/api/[mac].ts
@@ -1,17 +1,12 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
 import oui from "oui";
 
 type Data = {
-  name: string
-}
+  name: string;
+};
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  const mac = req.url?.toString().replace(/\/api\//g, "") ?? "";
-
-  const macInfo = oui(mac);
-  res.status(200).send(macInfo.split("\n")[0] ?? "MAC Not Found");
+export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+  const mac = oui(req.url?.toString().replace(/\/api\//g, "")) ?? "";
+  res.status(200).send(mac.split("\n")[0] ?? "MAC Not Found");
 }
