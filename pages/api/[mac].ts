17247b975938604edd3d22f13dab2f19890e5a17
Silent Spud <1478389+SilentSpud@users.noreply.github.com>
2024-10-02 19:09:03 -0600

    return 404 when not found

--- pages/api/[mac].ts
+++ pages/api/[mac].ts
@@ -2,12 +2,15 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import oui from "oui";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  let mac = "";
   try {
-    mac = oui(req.url?.toString().replace(/\/api\//g, "")).split("\n")[0] ?? "MAC Not Found";
+    const mac = oui(req.url?.toString().replace(/\/api\//g, "")).split("\n")[0];
+    if (mac) {
+      res.status(200).send(mac);
+    } else {
+      res.status(404).send("MAC Not Found");
+    }
   }
   catch (e) {
-    mac = "MAC Not Found";
+    res.status(404).send("MAC Not Found");
   }
-  res.status(200).send(mac);
 }
